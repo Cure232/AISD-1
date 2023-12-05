20131770dc208232a9e4ff00d6eed195616c0a62
Cure232 <timtim2004232@gmail.com>
2023-12-05 05:25:26 +0400

    Create constructors, start creating operators

---
+++ AISD-1.cpp
@@ -0,0 +1,19 @@
+﻿// AISD-1.cpp: определяет точку входа для приложения.
+//
+
+#include "AISD-1.h"
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int vals[3];
+	vals[0] = 1;
+	vals[1] = 2;
+	vals[2] = 3;
+	Vector<int> v = Vector<int>(3, vals);
+	cout << v[0] << " " << v[1] << " " << v[2] << endl;
+	return 0;
+}
---
+++ AISD-1.h
@@ -0,0 +1,129 @@
+﻿// AISD-1.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include <random>
+#include <complex>
+
+
+
+template <typename T>
+class Vector {
+	int _dimension;
+	T* _values;
+public:
+	int getDimension() {
+		return _dimension;
+	}
+
+	int vectorModule() {
+		T sum;
+		for (int i = 0; i < _dimension; i++) {
+			T x = _values[i];
+			sum += x * x;
+		}
+		return sqrt(sum);
+	}
+
+	Vector(int dimension) {
+		_dimension = dimension;
+		_values = new T[dimension];
+		for (int i = 0; i < dimension; i++) {
+			_values[i] = 0;
+		}
+	}
+
+	Vector(int dimension, T values[]) {
+		_dimension = dimension;
+		_values = new T[dimension];
+		for (int i = 0; i < dimension; i++) {
+			_values[i] = values[i];
+		}
+	}
+	Vector(int dimension, double minValue, double maxValue) { 
+		random_device rd;
+		mt19937 e{ rd() }; // or std::default_random_engine e{rd()};
+		uniform_real_distribution<double> dist{ minValue, maxValue };
+
+		_dimension = dimension;
+		_values = new T[dimension];
+		for (int i = 0; i < dimension; i++) {
+			_values[i] = dist(e);
+		}
+	}
+	
+	~Vector() {
+		delete(_values);
+	}
+
+	T& operator[](int i) {
+		return _values[i];
+	}
+	const T& operator[](int i) const {
+		return _values[i];
+	}
+
+	Vector operator+(const Vector& rhs) const {
+		if (_dimension == rhs._dimension)
+		{
+			Vector v(_dimension);
+			for (int i = 0; i < _dimension; i++) {
+				v[i] = _values[i] + rhs[i];
+			}
+			return v;
+		}
+		else
+		{
+			throw invalid_argument("Vectors have diffrent dimensions");
+		}
+	}
+	Vector operator-(const Vector& rhs) const {
+		if (_dimension == rhs._dimension)
+		{
+			Vector v(_dimension);
+			for (int i = 0; i < _dimension; i++) {
+				v[i] = _values[i] - rhs[i];
+			}
+			return v;
+		}
+		else
+		{
+			throw invalid_argument("Vectors have diffrent dimensions");
+		}
+	}
+	T operator*(const Vector& rhs) const {
+		if (_dimension == rhs._dimension)
+		{
+			T sum;
+			for (int i = 0; i < _dimension; i++) {
+				sum += _values[i] * rhs[i];
+			}
+			return sum;
+		}
+		else
+		{
+			throw invalid_argument("Vectors have diffrent dimensions");
+		}
+	}
+	Vector operator/(const Vector& rhs) const {
+
+	}
+
+	Vector operator+(const T& rhs) const {
+
+	}
+	Vector operator-(const T& rhs) const {
+
+	}
+	Vector operator*(const T& rhs) const {
+
+	}
+	Vector operator/(const T& rhs) const {
+
+	}
+};
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
